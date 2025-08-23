03ae92e62cddd32edf9effe009c878476adfb3f0
driver <driver.studio.89@gmail.com>
2025-08-23 08:22:14 +0300

    implementing feature cannot add events to vehicle that is not yours

--- src/main/java/com/driver/myvehiclelogger/service/impl/EventServiceImpl.java
+++ src/main/java/com/driver/myvehiclelogger/service/impl/EventServiceImpl.java
@@ -6,10 +6,12 @@ import com.driver.myvehiclelogger.mapper.EventMapper;
 import com.driver.myvehiclelogger.model.Event;
 import com.driver.myvehiclelogger.model.Vehicle;
 import com.driver.myvehiclelogger.service.EventService;
+import com.driver.myvehiclelogger.service.auth.UserAuthService;
 import com.driver.myvehiclelogger.web.dto.AddEventRequest;
 import com.driver.myvehiclelogger.web.dto.EventDto;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -19,12 +21,16 @@ public class EventServiceImpl implements EventService {
     private final EventMapper eventMapper;
     private final EventRepository eventRepository;
     private final VehicleRepository vehicleRepository;
+    private final UserAuthService userAuthService;
 
     @Override
     @Transactional
     public EventDto addEvent(AddEventRequest addEventRequest, Long vehicleId) {
-        Event event = eventMapper.toEntity(addEventRequest);
         Vehicle vehicle = vehicleRepository.findVehicleById(vehicleId).orElse(null);
+        if (vehicle.getOwnerId() != userAuthService.getCurrentUser().getId()) {
+            throw new AccessDeniedException("Access denied");
+        }
+        Event event = eventMapper.toEntity(addEventRequest);
         event.setVehicle(vehicle);
         Event savedEvent = eventRepository.save(event);
         vehicle.getEvents().add(savedEvent);
