ef746b51c2fdd414610414e893ddbc7d9222fdd5
driver <driver.studio.89@gmail.com>
2025-09-09 23:01:40 +0300

    remove dotenv and use local variable because i dont upload .env to github

--- src/main/java/com/driver/myvehiclelogger/config/Config.java
+++ src/main/java/com/driver/myvehiclelogger/config/Config.java
@@ -1,17 +1,20 @@
 package com.driver.myvehiclelogger.config;
 
 import com.cloudinary.Cloudinary;
-import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class Config {
 
+    @Value("${CLOUDINARY_URL:default-secret-key}")
+    private String cloudinaryUrl;
+
     @Bean
     public Cloudinary cloudinary() {
-        Dotenv dotenv = Dotenv.load();
-        Cloudinary cloudinary = new Cloudinary(dotenv.get("CLOUDINARY_URL"));
+
+        Cloudinary cloudinary = new Cloudinary(cloudinaryUrl);
         cloudinary.config.secure = true;
         return cloudinary;
     }
